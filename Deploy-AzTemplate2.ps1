9d1d4f05174262ed39f7125d9af3bc28dfd5e568
Stanley Chen <chen@pde.com>
2023-11-29 21:00:38 -0800

    test

--- Deploy-AzTemplate2.ps1
+++ Deploy-AzTemplate2.ps1
@@ -1,4 +1,4 @@
 az login --use-device-code
 
 az group create --name myResourceGroup --location "eastus"
-az deployment group create --resource-group myResourceGroup --parameters webAppName="AppServiceDemo" linuxFxVersion="PYTHON|3.7" --template-file "AppServiceARM.json"
+az deployment group create --resource-group myResourceGroup --parameters webAppName="AppServiceDemo" linuxFxVersion='PYTHON' --template-file "AppServiceARM.json"
